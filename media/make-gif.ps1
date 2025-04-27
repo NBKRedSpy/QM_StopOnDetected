067989182eb18a3311e646b2e410d1d0ba7e526d
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2025-04-26 21:01:56 -0400

    Added Example use

---
+++ media/make-gif.ps1
@@ -0,0 +1,9 @@
+# Origional version from https://superuser.com/questions/556029/how-do-i-convert-a-video-to-gif-using-ffmpeg-with-reasonable-quality#556031 
+#
+# ffmpeg -ss 30 -t 3 -i input.mp4 \
+#     -vf "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" \
+#     -loop 0 output.gif
+
+
+# x:\ffmpeg.exe -i .\MoveDemo.mp4 -vf "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 output.gif
+x:\ffmpeg.exe -i .\StoppedExample.mp4 -y -vf "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 .\StoppedExample.gif
